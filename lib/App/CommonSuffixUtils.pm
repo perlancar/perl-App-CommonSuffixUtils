2e831c612347854cf8e3d7ed2b089326a4ca0635
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-16 13:58:22 +0700

    Initial commit

---
+++ lib/App/CommonSuffixUtils.pm
@@ -0,0 +1,31 @@
+package App::CommonPrefixUtils;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Utilities related to common prefix
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+See the included scripts.
+
+
+=head1 DESCRIPTION
+
+This distribution includes the following CLI scripts related to common prefix.
+
+
+=head1 SEE ALSO
+
+L<String::CommonPrefix>
+
+L<App::CommonSuffixUtils>
+
+=cut
